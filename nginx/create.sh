65b379bf76eb9e56611a36759197aadc4abf444d
Cory Evans <cory0x45@gmail.com>
2020-07-07 21:53:14 +1200

    Added nginx/letsencrypt

---
+++ nginx/create.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+IMAGE=linuxserver/letsencrypt:latest
+CONTAINER_NAME=nginx
+DOMAIN=corye.me
+
+STAGING=true
+
+WORK_DIR=$(pwd)
+config_dir="$WORK_DIR/config"
+
+[ ! -d $config_dir ] && mkdir -p $config_dir
+
+docker create \
+    --name=$CONTAINER_NAME \
+    --cap-add=NET_ADMIN \
+    -e PUID=1000 \
+    -e PGID=1000 \
+    -e TZ=Pacific/Auckland \
+    -e URL=$DOMAIN \
+    -e SUBDOMAINS=www, \
+    -e VALIDATION=http \
+    -e STAGING=$STAGING \
+    -p 443:443 \
+    -p 80:80 \
+    -v $config_dir:/config \
+    --restart unless-stopped \
+    $IMAGE
+
+docker start $CONTAINER_NAME
